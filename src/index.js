2b4a139efded9b96035c2ae954ce7f9846faa5bc
JOELBUR19 <joelburitru@hotmail.com>
2025-11-26 11:28:34 -0500

    feat: protect tasks routes with jwt middleware

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import authRoutesLogin from "./controllers/login.js";
+import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Importar rutas
+import authRoutesRegister from "./controllers/register.js";
+import authRoutesLogin from "./controllers/login.js";
+
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+// Rutas de autenticación
+app.use("/auth", authRoutesRegister);
 app.use("/auth", authRoutesLogin);
+
 app.get("/", (req, res) => {
-  res.send("Api running")
-})
+  res.send("Api running");
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
