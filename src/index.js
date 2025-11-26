269cb891979aaae0dad6c98dc65b01b3cedaffbf
JOELBUR19 <yburitru@gmail.com>
2025-11-26 11:30:22 -0500

    feat: protect tasks routes with jwt middleware
    * Fix: eliminate unnecessary files
    
    * feat: protect tasks routes with jwt middleware
    
    ---------
    
    Co-authored-by: JOELBUR19 <joelburitru@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import express from "express";
-import authRoutes from "./controllers/register.js";
-import authRoutesLogin from "./controllers/login.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Importar rutas
+import authRoutesRegister from "./controllers/register.js";
+import authRoutesLogin from "./controllers/login.js";
+
 const app = express();
 const PORT = 3000;
+
 app.use(express.json());
 
-app.use("/auth", authRoutes);
+// Rutas de autenticación
+app.use("/auth", authRoutesRegister);
 app.use("/auth", authRoutesLogin);
+
 app.get("/", (req, res) => {
-  res.send("Api running")
-})
+  res.send("Api running");
+});
 
 app.listen(PORT, () => {
   console.log(`http://localhost:${PORT}`);
